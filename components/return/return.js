a6f5090762280a628b08bbd8471b9213636d3f9b
hez <626815494@qq.com>
2018-10-16 18:27:21 +0800

    日常

---
+++ components/return/return.js
@@ -0,0 +1,28 @@
+// pages/component/return/return.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 返回
+    fhs: function () {
+      wx.navigateBack({
+        delta: 2
+      })
+    },
+  }
+})
--- pages/shell/shell.js
+++ pages/shell/shell.js
@@ -8,6 +8,12 @@ Page({
 
   },
 
+  onGetShel: function(){
+    wx.navigateTo({
+      url: '../../pages/getShell/getShell',
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
